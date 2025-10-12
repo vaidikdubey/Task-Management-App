5e0197437b94183a200ad3f129adb37b081ff056
Vaidik Dubey <vaidik26dubey@gmail.com>
2025-10-12 22:23:27 +0530

    Updated healthcheck controller

--- src/controllers/healthcheck.controllers.js
+++ src/controllers/healthcheck.controllers.js
@@ -1,9 +1,19 @@
+import mongoose from "mongoose";
 import { ApiResponse } from "../utils/api-response.js";
+import { asyncHandler } from "../utils/async-handler.js";
 
-const healthCheck = (req, res) => {
-    res.status(200).json(
-        new ApiResponse(200, {message: "Server is up and running"})
-    )
-}
+const healthCheck = asyncHandler(async (req, res) => {
+    const dbState = mongoose.connection.readyState
+
+    const dbStatesArray = ["Disconnected", "Connected", "Connecting", "Disconnecting"];
+
+    return res.status(200)
+        .json(new ApiResponse(200, {
+            status: 200,
+            uptime: process.uptime().toFixed(0) + "s",
+            database: dbStatesArray[dbState],
+            timestamp: new Date().toISOString()
+    }, "Server is healthy🚀"))
+})
 
 export { healthCheck };
