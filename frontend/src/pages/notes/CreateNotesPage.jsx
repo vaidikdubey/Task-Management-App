ac5bc2e6ac5129b19301450b7d6a0b284029ed38
Vaidik Dubey <vaidik26dubey@gmail.com>
2025-12-31 19:44:33 +0530

    Added back to dashboard button

--- frontend/src/pages/notes/CreateNotesPage.jsx
+++ frontend/src/pages/notes/CreateNotesPage.jsx
@@ -1,9 +1,17 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import { ArrowBigLeft } from "lucide-react";
 
 export const CreateNotesPage = () => {
     return (
-        <div className="flex justify-center items-center text-2xl font-bold min-h-full w-full py-50 my-15 text-amber-400 shadow-xl rounded-2xl">
-            Feature under construction.... We regret the inconvenience😔
-        </div>
+        <>
+            <Link to={"/"}>
+                <ArrowBigLeft className="absolute left-5 top-5 text-emerald-400 cursor-pointer hover:text-sky-500 hover:scale-110" />
+            </Link>
+
+            <div className="flex justify-center items-center text-2xl font-bold min-h-full w-full py-50 my-15 text-amber-400 shadow-xl rounded-2xl">
+                Feature under construction.... We regret the inconvenience😔
+            </div>
+        </>
     );
 };
